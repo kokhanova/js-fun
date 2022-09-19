9de3b7d1030ced785626c9a64638de03b38b072b
kokhanova <konevao@list.ru>
2022-09-19 13:45:59 +0300

    added date calculation

--- script.js
+++ script.js
@@ -12,11 +12,37 @@ while(n--) {
     data.push(getNumber(100))
 }
 
-console.log(data);
 
 const box = document.querySelector(".box");
+box.innerHTML = "<div class=\"card\"></div>"
 
 for(let val of data) {
     box.innerHTML += `<div class="card"
     style="background-color: ${colors[getNumber(colors.length - 1)]}">${optimizeNumber(val)}</div>`;
 }
+const nullish = (value, str) => {
+    if (value === 0) return '';
+    return `${value} ${str}`;
+}
+
+function showTime(day = new Date(2023, 1, 7, 23)) {
+    let now = new Date();
+    let d, h, m, s;
+    let time = Math.floor((day - now) / 1000);
+
+    m = Math.floor(time / 60);
+    s = time - m * 60;
+    // s = 2
+    h = Math.floor(m / 60);
+    m -= h * 60;
+    d = Math.floor(h / 24);
+    h -= d *24;
+    return `${nullish(d, 'дн.')}  ${nullish(h, 'ч.')}  ${nullish(m, 'мин.')}  ${nullish(s, 'сек.')} `;
+}
+
+setInterval(function(){
+    box.firstElementChild.innerText = `До завершения обучения осталось: ${showTime()}`;
+});
+
+
+
